64af9ae56affaaa52c6a132095ce8b6a85cd4bf5
baran <dasdemirbaran@gmail.com>
2019-03-09 22:12:42 +0300

    fixed register & did some refactoring

--- routes/login.js
+++ routes/login.js
@@ -4,28 +4,30 @@ const db = require('../lib/db/queries');
 const util = require('../lib/util');
 const bcrypt = require('bcrypt');
 
+// mounts to /login
+router.get('/', (req, res, next) => {
+    const activeSession = (req.session.userid) ? req.session : -1;
+    res.render('login', {
+        title: 'Zalina | Oturum aç',
+        activeSession,
+        wrongPass: 0
+    });
+});
 
-
-router.post('/userlogin', (req, res, next) => {
+router.post('/', (req, res, next) => {
     if (!req.session.active) {
         db.getUserByMail(req.body.email).then(result => {
-            if (result.length > 0 && req.body.password === result[0].password) {
-                // if (bcrypt.hashSync(req.body.password, bcrypt.genSaltSync()) === result.password) {
+            if (result.length > 0 && bcrypt.compareSync(req.body.password, result[0].password)) {
                 req.session.email = result[0].email;
                 req.session.userid = result[0].id;
                 res.redirect('/');
-                //}
-                //else {
-                //res.send('hatalı giiş');
-                //}
+            } else {
+                res.render('login', {
+                    title: 'Zalina | Oturum aç',
+                    activeSession: -1,
+                    wrongPass: 1
+                });
             }
-
-            else {
-                res.render('login', { title: 'Zalina | Oturum aç', activeSession: -1, wrongPass: 1 });
-
-
-            }
-
         })
     }
 });
