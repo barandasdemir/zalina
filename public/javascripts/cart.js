b115c557ce405f552c8c5e6fd4f4e9cabae23824
baran <dasdemirbaran@gmail.com>
2019-04-21 02:07:25 +0300

    added size chart to product page

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -25,27 +25,6 @@ $(document).ready(function () {
             });
     });
 
-    $(".button-buy").click(() => {
-        const loc = window.location.href.split('/');
-        const selects = document.querySelectorAll('select');
-        const size = M.FormSelect.getInstance(selects[0]).$selectOptions.filter(opt => opt.selected)[0].innerText;
-        $.ajax({
-                method: 'POST',
-                url: `/cart/add`,
-                data: {
-                    id: loc[loc.length - 1],
-                    size
-                }
-            })
-            .done(() => {
-                $('#cartQty')[0].innerText = ++cartQty;
-                M.toast({
-                    html: `'${$('.productName')[0].innerText}' alışveriş sepetinize eklendi!`,
-                    classes: 'rounded'
-                });
-            });
-    });
-
     $('.qtyMinus').click((event) => {
         const item = event.target.parentElement.parentElement.parentElement.parentElement;
         const itemQty = event.target.parentElement.children[1];
